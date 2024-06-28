e2e802ea0ca59dbf02f63a232d54fc2c34713c90
savewaris <savewaris@gmail.com>
2024-06-28 13:09:15 +0700

    Create math.js

---
+++ math.js
@@ -0,0 +1,15 @@
+function sum(a,b){
+    return a+b;
+}
+
+ function multiply(a,b){
+    return a*b;
+ }
+ function divide(a, b){
+    if (b === 0) {
+        throw new Error('Cannot divide by zero');
+    }
+    return a / b;
+ }
+
+ module.exports = { sum,multiply,divide};
