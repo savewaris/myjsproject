73cdae62280f82cbbdb77e3444fa5dce50aa687a
savewaris <savewaris@gmail.com>
2024-06-28 13:30:43 +0700

    Update calculator.test.js

--- calculator.test.js
+++ calculator.test.js
@@ -3,3 +3,11 @@ const calculate = require('./calculator');
 test ('1+2 * 4 / 2',()=>{
     expect(calculate(1,2,4)).toBe(6);
 });
+
+test ('2+4 * 6 / 2',()=>{
+    expect(calculate(2,4,6)).toBe(18);
+});
+
+test ('4+6 * 8 / 2',()=>{
+    expect(calculate(4,6,8)).toBe(40);
+});
