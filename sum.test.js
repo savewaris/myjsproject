5f4704b5e32bdefaca8e3fdf228330b1fe2c819a
savewaris <129939151+savewaris@users.noreply.github.com>
2024-06-28 11:53:38 +0700

    Update sum.test.js

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,11 @@ const sum = require('./sum');
 test('', () => {
     expect(sum(1,2)).toBe(3);
 })
+
+test('', () => {
+    expect(sum(5,10)).toBe(15);
+})
+
+test('', () => {
+    expect(sum(13,5)).toBe(18);
+})
